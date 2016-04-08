ef223ec318e7bd98bfc8a776d7fcbea8dc1af0fb
Parker Riebs (UWL) <riebs.park@uwlax.edu>
2016-04-07 22:40:16 -0500

    Added header comments to functions.

--- scheduler.h
+++ scheduler.h
@@ -42,14 +42,71 @@
 /******************************
  * Function declarations
  ******************************/
+ /*
+ *  Runs and prints First-Come-First-Serve scheduling algorithm with process 
+ *  information.
+ *
+ *  Parameters:
+ *   process_info: The 2d array of process information that can be sorted.
+ *   orig_process: The 2d array of process information that preserves process
+ *                 order
+ *   num_process: Number of processes in process_info and orig_process.
+ */
 void first_come_first_serve(int ** process_info, int ** orig_process, int num_process);
 
+/*
+*  Runs and prints Shortest Job First scheduling algorithm with process
+*  information.
+*
+*  Parameters:
+*   process_info: The 2d array of process information that can be sorted.
+*   orig_process: The 2d array of process information that preserves process
+*                 order
+*   num_process: Number of processes in process_info and orig_process.
+*/
 void shortest_job_first(int ** process_info, int ** orig_process, int num_process);
 
+/*
+*  Runs and prints Priority scheduling algorithm with process
+*  information.
+*
+*  Parameters:
+*   process_info: The 2d array of process information that can be sorted.
+*   orig_process: The 2d array of process information that preserves process
+*                 order
+*   num_process: Number of processes in process_info and orig_process.
+*/
 void priority(int ** process_info, int ** orig_process, int num_process);
 
+/*
+*  Runs and prints Round Robin scheduling algorithm with process
+*  information.
+*
+*  Parameters:
+*   process_info: The 2d array of process information that can be sorted.
+*   num_process: Number of processes in process_info and orig_process.
+*   quantum: The amount of time for each process cycle.
+*/
 void round_robin(int ** process_info, int num_process, int quantum);
 
+/*
+*  Sorts a 2d array of process information with bubble sort.
+*
+*  Parameters:
+*   process_info: The 2d array of process information that can be sorted.
+*   num_process: Number of processes in process_info and orig_process.
+*   sort_column: The column of the process information in which to sort.
+*/
 void bubble_sort(int ** process_info, int num_process, int sort_column);
 
+/*
+*  Prints all process information and extra scheduling algorithm data.
+*
+*  Parameters:
+*   process_info: The 2d array of process information that can be sorted.
+*   num_process: Number of processes in process_info and orig_process.
+*   total_wait: The total amount of time waited by each process.
+*   total_turnaround: The total amount of time it took for each process to
+*                     turnaround.
+*/
 void print_results(int ** process_info, int num_process, double total_wait, double total_turnaround);
